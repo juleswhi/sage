3ed455a3c3550e130b63c769027aba8f2c42c8cc
jules white <jules.white1@protonmail.com>
2024-09-02 22:27:08 +0100

    Add init to window struct

--- game/main.zig
+++ game/main.zig
@@ -2,13 +2,12 @@ const std = @import("std");
 const sage = @import("sage");
 const log = @import("sage").log;
 const ev = @import("sage").event;
-const init_glfw = @import("sage").init_glfw;
 
 pub fn main() !void {
     const alloc = std.heap.page_allocator;
     _ = try ev.new(ev.event_type.AppTick, "KEY_PRESSED", &alloc);
 
-    init_glfw();
+    sage.window.init_glfw("Sage Engine", 800, 400);
 }
 
 fn entry() void {}
--- sage/window/window.zig
+++ sage/window/window.zig
@@ -10,31 +10,31 @@ pub const window = struct {
     height: u32,
 
     set_event_callback: ?*fn (callback: fn (e: *const event) void) void,
+
+    pub fn init_glfw(title: [*:0]const u8, width: u32, height: u32) void {
+        glfw.setErrorCallback(errorCallback);
+
+        if (!glfw.init(.{})) {
+            std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+            std.process.exit(1);
+        }
+
+        defer glfw.terminate();
+
+        const w = glfw.Window.create(width, height, title, null, null, .{}) orelse {
+            std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+            std.process.exit(1);
+        };
+
+        defer w.destroy();
+
+        while (!w.shouldClose()) {
+            w.swapBuffers();
+            glfw.pollEvents();
+        }
+    }
+
+    fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+        std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+    }
 };
-
-pub fn init_glfw() void {
-    glfw.setErrorCallback(errorCallback);
-    if (!glfw.init(.{})) {
-        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    }
-    defer glfw.terminate();
-
-    // Create our window
-    const w = glfw.Window.create(400, 400, "Sage Engine", null, null, .{}) orelse {
-        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
-        std.process.exit(1);
-    };
-
-    defer w.destroy();
-
-    // Wait for the user to close the window.
-    while (!w.shouldClose()) {
-        w.swapBuffers();
-        glfw.pollEvents();
-    }
-}
-
-fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
-    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
-}
